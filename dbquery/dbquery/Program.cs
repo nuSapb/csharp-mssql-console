84b2703a9be06e671631f206a684049d8000649c
Anu Sakpibal <Anu.Sakpibal@kimballelectronics.com>
2021-09-14 17:19:20 +0700

    init project

---
+++ dbquery/dbquery/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace dbquery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getting Connection ...");
+
+            string connectionString = null;
+            connectionString = @"Data Source=SQL205\PSQL205;Initial Catalog=KETL_Tester;User ID=TesterFullAccess;Password=Tester2Full!;Connection Timeout=60;";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                Console.WriteLine("Opening Connection ...");
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
